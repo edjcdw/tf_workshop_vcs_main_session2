d9fcd17d62e4ec372fe331d4ec84807a7bc77d36
ed <eddavisj@gmail.com>
2024-08-07 13:51:14 -0400

    initial

---
+++ main.tf
@@ -0,0 +1,20 @@
+module "virtualnetwork" {
+  source  = "app.terraform.io/tsc_demo/virtualnetwork/azurerm"
+  version = "1.0.0"
+  name                = "example-vnet"
+  resource_group_name = "example-rg"
+  location            = "eastus"
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+  tags                = {
+    Environment = "Development"
+    Project     = "Example"
+  }
+
+  # Optional variables
+  ddos_protection_plan_id = null
+  encryption              = "DropUnencrypted"
+  hub_vnet_name           = "hub-vnet"
+  hub_rg_name             = "hub-rg"
+  use_remote_gateway      = true
+}
