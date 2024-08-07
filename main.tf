df8135fc4f70f87d298c0d916e47e8235cae306e
ed <eddavisj@gmail.com>
2024-08-07 14:14:07 -0400

    update vnet provider

--- main.tf
+++ main.tf
@@ -12,6 +12,11 @@ module "virtualnetwork" {
   location            = var.location
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  providers = {
+    azurerm = azurerm
+    azurerm.hub = azurerm
+  }
   tags                = {
     Environment = "Development"
     Project     = "Example"
